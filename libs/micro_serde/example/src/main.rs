b0d6b6feec108db65f7a780bd0acd3e5fb8f01df
Athan Xiao <Athan.Xiao@one.nz>
2026-08-26 14:16:18 +1200

    update serde

--- libs/micro_serde/example/src/main.rs
+++ libs/micro_serde/example/src/main.rs
@@ -1,8 +1,10 @@
-// lets make tinyserde dep free too!
+// micro_serde is dependency-free: it ships its own binary (SerBin/DeBin) and
+// JSON (SerJson/DeJson) codecs, no serde. (The RON codec was removed, so this
+// example no longer exercises SerRon/DeRon.)
 
 use makepad_micro_serde::*;
 
-#[derive(SerBin, DeBin, SerJson, DeJson, SerRon, DeRon, PartialEq)]
+#[derive(SerBin, DeBin, SerJson, DeJson, PartialEq)]
 struct MyStruct<T> where T: Clone {
     pub a: T,
     b: u32,
@@ -25,9 +27,9 @@ struct RenamedStruct {
     #[rename(lastName)]
     last_name: String,
     age: u32,  // Not renamed
-} 
+}
 
-#[derive(SerBin, DeBin, SerJson, DeJson, SerRon, DeRon, PartialEq)]
+#[derive(SerBin, DeBin, SerJson, DeJson, PartialEq)]
 enum MyEnum<T> where T: Clone {
     One,
     Two(T, u32),
@@ -36,8 +38,6 @@ enum MyEnum<T> where T: Clone {
 }
 
 fn main() {
-    //let a = MyStruct{step1:1,step2:None};
-    //let x = MyStruct2(1,2);
     let x = MyStruct {
         a: 1,
         b: 2,
@@ -55,17 +55,12 @@ fn main() {
     println!("Bin len: {}", bin.len());
     let y:MyStruct<usize> = DeBin::deserialize_bin(&bin).unwrap();
     println!("Bin roundtrip equality {}", x == y);
-    
+
     let json = x.serialize_json();
     println!("JSON Output {}", json);
     let y:MyStruct<usize> = DeJson::deserialize_json(&json).unwrap();
     println!("JSON roundtrip equality {}", x == y);
-    
-    let ron = x.serialize_ron();
-    println!("RON Output {}", ron);
-    let y:MyStruct<usize> = DeRon::deserialize_ron(&ron).unwrap();
-    println!("RON roundtrip equality {}", x == y);
-    
+
     // Test #[rename] attribute
     let renamed = RenamedStruct {
         first_name: "John".to_string(),
@@ -75,10 +70,10 @@ fn main() {
     let renamed_json = renamed.serialize_json();
     println!("\nRenamed JSON Output: {}", renamed_json);
     // Should output: {"firstName":"John","lastName":"Doe","age":30}
-    
+
     let renamed_back: RenamedStruct = DeJson::deserialize_json(&renamed_json).unwrap();
     println!("Renamed roundtrip equality: {}", renamed == renamed_back);
-    
+
     // Test deserializing from external JSON with renamed fields
     let external_json = r#"{"firstName":"Jane","lastName":"Smith","age":25}"#;
     let from_external: RenamedStruct = DeJson::deserialize_json(external_json).unwrap();
